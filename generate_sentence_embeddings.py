f6284954c5e631e9647211bc2ef389aab825738d
Steve Toner <srtoner@umich.edu>
2023-04-07 00:03:53 -0400

    bounce back

--- generate_sentence_embeddings.py
+++ generate_sentence_embeddings.py
@@ -15,43 +15,49 @@ os.chdir(cwd)
 import gensim.downloader as api
 from multiprocessing import Pool, cpu_count
 
-# Load pre-trained GloVe embeddings
 glove_model = api.load('glove-wiki-gigaword-300')
 
-# Define the function to generate sentence embeddings
+ # Define the function to generate sentence embeddings
 def generate_embedding(sentence):
-    words = sentence.lower().split()
+    
+    words = " ".join(sentence).lower().split()
     word_embeddings = [glove_model[word] for word in words if word in glove_model]
     if not word_embeddings:
         return None
     sentence_embedding = sum(word_embeddings) / len(word_embeddings)
     return sentence_embedding
 
-data = U.load_file('data_vFF.pkl', 'pkl', config['DATADIR'])
-# data = data[:16]
-text_combined = list(itertools.chain.from_iterable([dat['text'] for dat in data]))
-passage_keys = list(itertools.chain.from_iterable([[dat['passage_key']]*len(dat['text']) for dat in data]))
-seq_idx = [i for i in range(len(text_combined))]
-
-zipped = list(zip(seq_idx, passage_keys, text_combined))
 
 
-# Tokenize and preprocess your corpus
-corpus = zipped
+if __name__ == '__main__':
+    # Load pre-trained GloVe embeddings
+   
 
-# Define the number of processes to use
-num_processes = cpu_count()
+   
+    data = U.load_file('data_vFF.pkl', 'pkl', config['DATADIR'])
+    # data = data[:16]
+    text_combined = list(itertools.chain.from_iterable([dat['text'] for dat in data]))
+    passage_keys = list(itertools.chain.from_iterable([[dat['passage_key']]*len(dat['text']) for dat in data]))
+    seq_idx = [i for i in range(len(text_combined))]
 
-# Split the corpus into chunks
-chunk_size = len(corpus) // num_processes
-chunks = [corpus[i:i+chunk_size] for i in range(0, len(corpus), chunk_size)]
+    zipped = list(zip(seq_idx, passage_keys, text_combined))
 
-# Generate sentence embeddings in parallel
-with Pool(num_processes) as pool:
-    sentence_embeddings = []
-    for chunk_embeddings in pool.imap(map, chunks):
-        sentence_embeddings.extend(chunk_embeddings)
 
-# Save sentence embeddings to file
-with open('sentence_embeddings.pkl', 'wb') as f:
-    pkl.dump(sentence_embeddings, f)
+    # Tokenize and preprocess your corpus
+    corpus = text_combined
+    # Define the number of processes to use
+    num_processes = cpu_count()
+
+    # Split the corpus into chunks
+    chunk_size = len(corpus) // num_processes
+    chunks = [corpus[i:i+chunk_size] for i in range(0, len(corpus), chunk_size)]
+
+    # Generate sentence embeddings in parallel
+    with Pool(num_processes) as pool:
+        sentence_embeddings = []
+        for chunk_embeddings in pool.imap(generate_embedding, chunks, chunk_size):
+            sentence_embeddings.extend(chunk_embeddings)
+
+    # Save sentence embeddings to file
+    with open('sentence_embeddings.pkl', 'wb') as f:
+        pkl.dump(sentence_embeddings, f)
